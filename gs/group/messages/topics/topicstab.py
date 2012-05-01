6673e3d2c365a040e4ab5d18f514ebc9695c1cac
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-02 09:51:02 +1000

    Moving the Topics tabe here, from gs.group.home

---
+++ gs/group/messages/topics/topicstab.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from zope.component import getMultiAdapter
+from Products.GSGroup.interfaces import IGSMailingListInfo
+from gs.group.member.canpost.interfaces import IGSPostingUser
+from gs.group.home.simpletab import UserInfoTab
+
+class TopicsTab(UserInfoTab):
+    def __init__(self, group, request, view, manager):
+        UserInfoTab.__init__(self, group, request, view, manager)
+            
+    @Lazy
+    def canPost(self):
+        retval = getMultiAdapter((self.groupInfo.groupObj, self.userInfo), 
+                                  IGSPostingUser)
+        return retval
+        
+    @Lazy
+    def email(self):
+        l = IGSMailingListInfo(self.groupInfo.groupObj)
+        retval = l.get_property('mailto')
+        return retval
+
