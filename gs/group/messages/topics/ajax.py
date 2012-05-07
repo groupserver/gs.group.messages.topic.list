d77c50b66ca155e9965cf84b41d26de51abc995d
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-07 11:10:26 +1000

    Read the offset and limit from the request.

--- gs/group/messages/topics/ajax.py
+++ gs/group/messages/topics/ajax.py
@@ -15,11 +15,25 @@ class TopicsAjax(GroupPage):
     def searchTokens(self):
         retval = createObject('groupserver.SearchTextTokens', "")
         return retval
-        
+    
+    @Lazy
+    def offset(self):
+        retval = int(self.request.get('i', 0))
+        assert retval >= 0
+        return retval
+    
+    @Lazy
+    def limit(self):
+        print dir(self)
+        print dir(self.request)
+        retval = int(self.request.get('l', 12)) % 48
+        assert retval >= 0
+        return retval
+    
     @Lazy
     def topics(self):
         retval = self.messageQuery.topic_search_keyword(
               self.searchTokens, self.siteInfo.id,
-              [self.groupInfo.id], limit=10, offset=0)
+              [self.groupInfo.id], limit=self.limit, offset=self.offset)
         return retval
 
