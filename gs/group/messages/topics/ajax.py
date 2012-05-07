7a77fb1ae61190997803ce3ddb270ccd816e8eaf
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-07 13:00:44 +1000

    Adding the keywords.

--- gs/group/messages/topics/ajax.py
+++ gs/group/messages/topics/ajax.py
@@ -1,10 +1,14 @@
 # coding=utf-8
+from math import log10
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from gs.group.base.page import GroupPage
 from Products.GSSearch.queries import MessageQuery
+from Products.XWFCore.cache import LRUCache
+from Products.XWFMailingListManager.stopwords import en as STOP_WORDS
 
 class TopicsAjax(GroupPage):
+    topicKeywords = LRUCache("TopicKeywords")
     @Lazy
     def messageQuery(self):
         da = self.context.zsqlalchemy
@@ -13,8 +17,7 @@ class TopicsAjax(GroupPage):
 
     @Lazy
     def searchTokens(self):
-        retval = createObject('groupserver.SearchTextTokens', "")
-        return retval
+        return createObject('groupserver.SearchTextTokens', "")
     
     @Lazy
     def offset(self):
@@ -30,10 +33,10 @@ class TopicsAjax(GroupPage):
     
     @Lazy
     def rawTopicInfo(self):
-        retval = self.messageQuery.topic_search_keyword(
-              self.searchTokens, self.siteInfo.id,
-              [self.groupInfo.id], limit=self.limit, offset=self.offset)
-        return retval
+        return self.messageQuery.topic_search_keyword(
+                self.searchTokens, self.siteInfo.id, 
+                [self.groupInfo.id], limit=self.limit, 
+                offset=self.offset)
         
     @Lazy
     def topicFiles(self):
@@ -50,10 +53,57 @@ class TopicsAjax(GroupPage):
                 if f['topic_id'] == topic['topic_id']]
         return retval
         
+    def keywords_for_topic(self, topic):
+        retval = self.topicKeywords.get(topic['last_post_id'])
+        if not retval:
+            retval = self.generate_keywords_for_topic(topic)
+            self.topicKeywords.add(topic['last_post_id'], retval)
+        return retval
+
+    @Lazy
+    def topicsWordCounts(self):
+        tIds = [t['topic_id'] for t in self.rawTopicInfo]
+        retval = self.messageQuery.topics_word_count(tIds)
+        return retval
+    
+    @Lazy
+    def totalTopicCount(self):
+        return self.messageQuery.count_topics()
+        
+    @Lazy
+    def wordCounts(self):
+        return self.messageQuery.word_counts()
+        
+    def generate_keywords_for_topic(self, topic):
+        tId = topic['topic_id']
+        topicWords = [tw for tw in self.topicsWordCounts 
+                        if tw['topic_id'] == tId]
+        twc = float(sum([w['count'] for w in topicWords]))
+        wc = self.wordCounts
+        retval = [{ 'word':  w['word'],
+                    'tfidf': (w['count']/twc)*\
+                              log10(self.totalTopicCount/\
+                                    float(wc.get('word', 1)))}
+                for w in topicWords
+                if ((len(w['word']) > 3) and 
+                     (w['word'] not in STOP_WORDS))]
+        retval.sort(tfidf_sort)
+        return retval
+
     def topics(self):
         '''Generator, which returns the topics'''
         topics = self.rawTopicInfo
         for topic in topics:
             topic['files'] = self.files_for_topic(topic)
+            topic['keywords'] = self.keywords_for_topic(topic)
             yield topic
+
+def tfidf_sort(a, b):
+    if a['tfidf'] < b['tfidf']:
+        retval = 1
+    elif a['tfidf'] == b['tfidf']:
+        retval = 0
+    else:
+        retval = -1
+    return retval
 
