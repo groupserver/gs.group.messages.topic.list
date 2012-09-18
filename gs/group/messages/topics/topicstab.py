5db289830665fc69d806d3a184fdfd37df03c699
Michael JasonSmith <mpj17@onlinegroups.net>
2012-09-19 09:46:02 +1000

    Code cleanup

--- gs/group/messages/topics/topicstab.py
+++ gs/group/messages/topics/topicstab.py
@@ -5,19 +5,19 @@ from Products.GSGroup.interfaces import IGSMailingListInfo
 from gs.group.member.canpost.interfaces import IGSPostingUser
 from gs.group.home.simpletab import UserInfoTab
 
+
 class TopicsTab(UserInfoTab):
     def __init__(self, group, request, view, manager):
         UserInfoTab.__init__(self, group, request, view, manager)
-            
+
     @Lazy
     def canPost(self):
-        retval = getMultiAdapter((self.groupInfo.groupObj, self.userInfo), 
+        retval = getMultiAdapter((self.groupInfo.groupObj, self.userInfo),
                                   IGSPostingUser)
         return retval
-        
+
     @Lazy
     def email(self):
         l = IGSMailingListInfo(self.groupInfo.groupObj)
         retval = l.get_property('mailto')
         return retval
-
