51bdca9654b7c44519cb32adf0a9cdfb803f4e70
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-15 16:44:40 +1100

    Cleanup of the search JavaScript

--- gs/group/messages/topics/browser/javascript/topicstab.js
+++ gs/group/messages/topics/browser/javascript/topicstab.js
@@ -1,27 +1,24 @@
 jQuery.noConflict();
 
-var init_topic_search = function () {
-    var topicsSearch = null;
-    var b = null;
-    var url = null;
+function init_topic_search() {
+    var topicsSearch = null, b = null, url = null, 
+        ts = '#gs-group-messages-topics-search';
 
     b = jQuery('base').attr('href');
     if (b[b.length -1] != '/') {
         b = b + '/';
     }
     url = b + 'gs-group-topics-ajax.html';
-    topicsSearch = GSSearch('#gs-group-messages-topics-search', 
-                            url, 0, 6, {}, null);
+    topicsSearch = GSSearch(ts, url, 0, 6, {}, null);
+    jQuery(ts).on('resultsloaded', 
+                  function(){jQuery('abbr[role=tooltip]').tooltip()});
+    jQuery('#gs-group-messages-base-tabs-buttons li:first-child a').click();
     topicsSearch.load();
-    jQuery('#gs-group-messages-topics-search').on('resultsloaded', function () {
-        jQuery('abbr[role=tooltip]').tooltip();
-        jQuery('#gs-group-messages-base-tabs-buttons li:first-child a').click();
-    });
 }
 
 // Connect to the DOM being ready, rather than the window loading, so the 
 // topics can load while all the other assets are being downloaded
 jQuery(document).ready(function () {
-    gsJsLoader.with_module('/++resource++gs-search-base-js-20121217.js',
+    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20121217.js',
                            init_topic_search);
 });
