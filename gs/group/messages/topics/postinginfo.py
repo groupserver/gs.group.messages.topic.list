bb1193eb4a188ef0e23757726eeacd8852a8253c
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-26 16:15:38 +1100

    Moving the posting info into its own viewlet.

--- gs/group/messages/topics/postinginfo.py
+++ gs/group/messages/topics/postinginfo.py
@@ -1,16 +1,28 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from zope.cachedescriptors.property import Lazy
 from zope.component import getMultiAdapter
+from Products.GSGroup.interfaces import IGSMailingListInfo
 from gs.group.member.canpost.interfaces import IGSPostingUser
 from gs.group.home.simpletab import UserInfoTab
 
 
-class TopicsTab(UserInfoTab):
+class PostingInfo(UserInfoTab):
     def __init__(self, group, request, view, manager):
-        UserInfoTab.__init__(self, group, request, view, manager)
+        super(PostingInfo, self).__init__(group, request, view, manager)
+
+    @Lazy
+    def show(self):
+        retval = self.canPost.canPost
+        return retval
 
     @Lazy
     def canPost(self):
         retval = getMultiAdapter((self.groupInfo.groupObj, self.loggedInUser),
                                     IGSPostingUser)
+        return retval
+
+    @Lazy
+    def email(self):
+        l = IGSMailingListInfo(self.groupInfo.groupObj)
+        retval = l.get_property('mailto')
         return retval
