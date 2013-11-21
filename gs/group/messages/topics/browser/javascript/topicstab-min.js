d783810051df983650315a8c630ae3eb1bdb13b5
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-21 11:19:10 -0800

    Adding a minified version of the JS.

---
+++ gs/group/messages/topics/browser/javascript/topicstab-min.js
@@ -0,0 +1,6 @@
+jQuery.noConflict();function init_topic_search(){var c=null,a=null,d=null,e="#gs-group-messages-topics-search";
+a=jQuery("base").attr("href");if(a[a.length-1]!="/"){a=a+"/"}d=a+"gs-group-topics-ajax.html";
+c=GSSearch(e,d,0,6,{},null);jQuery(e).on("resultsloaded",function(){jQuery("abbr[role=tooltip]").tooltip()
+});jQuery("#gs-group-messages-base-tabs-buttons li:first-child a").click();c.load()
+}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20131121.js",init_topic_search)
+});
