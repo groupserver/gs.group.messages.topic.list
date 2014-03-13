152432b4f34c3d3ba0e0eec8a7d7d8e167a75872
Michael JasonSmith <mpj17@onlinegroups.net>
2014-03-13 15:30:24 -0700

    Using strict-mode for the JS, following the update to the core search-code, and updating the version number.

--- gs/group/messages/topics/browser/javascript/topicstab-min.js
+++ gs/group/messages/topics/browser/javascript/topicstab-min.js
@@ -1,8 +1,8 @@
-jQuery.noConflict();function init_topic_search(){var c=null,a=null,d=null,e="#gs-group-messages-topics-search";
+"use strict";jQuery.noConflict();function init_topic_search(){var c=null,a=null,d=null,e="#gs-group-messages-topics-search";
 a=jQuery("base").attr("href");if(a[a.length-1]!="/"){a=a+"/"}d=a+"gs-group-topics-ajax.html";
 c=GSSearch(e,d,0,6,{},null);jQuery(e).on("resultsloaded",function(){jQuery("abbr[role=tooltip]").tooltip()
 });jQuery("#gs-group-messages-base-tabs-buttons li:first-child a").click();c.load()
-}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20131121.js",init_topic_search)
-});jQuery(window).load(function(){var a=null,b=null;if(jQuery("#gs-group-messages-topics-postinginfo").length!=0){a=jQuery("#gs-group-messages-topics-postinginfo-privacy").html();
-b={animation:true,html:true,placement:"bottom",trigger:"click",content:a};jQuery("#gs-group-messages-topics-postinginfo-privacy-summary").popover(b)
-}});
+}jQuery(document).ready(function(){gsJsLoader.with_module("/++resource++gs-search-base-js-min-20140313.js",init_topic_search)
+});jQuery(window).load(function(){var b=null,c=null,a=null;if(jQuery("#gs-group-messages-topics-postinginfo").length!=0){b=jQuery("#gs-group-messages-topics-postinginfo-privacy").html();
+c={animation:true,html:true,placement:"bottom",trigger:"click",content:b};a=jQuery("#gs-group-messages-topics-postinginfo-privacy-summary");
+a.popover(c)}});
--- gs/group/messages/topics/browser/javascript/topicstab.js
+++ gs/group/messages/topics/browser/javascript/topicstab.js
@@ -1,4 +1,5 @@
-// GroupServer JavaScript module for providing the Search mechanism
+"use strict";
+// GroupServer JavaScript module for providing contents of the Topics tab
 //
 // Copyright © 2014 OnlineGroups.net and Contributors.
 // All Rights Reserved.
@@ -19,7 +20,7 @@ function init_topic_search() {
     url = b + 'gs-group-topics-ajax.html';
     topicsSearch = GSSearch(ts, url, 0, 6, {}, null);
     jQuery(ts).on('resultsloaded',
-                  function(){jQuery('abbr[role=tooltip]').tooltip()});
+                  function(){jQuery('abbr[role=tooltip]').tooltip();});
     jQuery('#gs-group-messages-base-tabs-buttons li:first-child a').click();
     topicsSearch.load();
 }
@@ -27,16 +28,20 @@ function init_topic_search() {
 // Connect to the DOM being ready, rather than the window loading, so the
 // topics can load while all the other assets are being downloaded
 jQuery(document).ready(function () {
-    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20131121.js',
+    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20140313.js',
                            init_topic_search);
 });
 
 jQuery(window).load( function () {
-    var h=null, d=null;
+    var h=null, d=null, s=null;
     if (jQuery('#gs-group-messages-topics-postinginfo').length != 0) {
         h = jQuery('#gs-group-messages-topics-postinginfo-privacy').html();
-        d = {animation: true, html: true, placement: 'bottom', 
-             trigger: 'click', content: h};
-        jQuery('#gs-group-messages-topics-postinginfo-privacy-summary').popover(d);
+        d = {animation: true,
+             html: true,
+             placement: 'bottom',
+             trigger: 'click',
+             content: h};
+        s = jQuery('#gs-group-messages-topics-postinginfo-privacy-summary');
+        s.popover(d);
     }
 });
