07f77c29adafe9b2ff95af8e7bdb509737197e08
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-01 11:38:12 -0800

    Whitespace cleanup for gjslint

--- gs/group/messages/topic/list/browser/javascript/topicstab.js
+++ gs/group/messages/topic/list/browser/javascript/topicstab.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 // GroupServer JavaScript module for providing contents of the Topics tab
 //
-// Copyright © 2014 OnlineGroups.net and Contributors.
+// Copyright © 2014, 2016 OnlineGroups.net and Contributors.
 // All Rights Reserved.
 //
 // This software is subject to the provisions of the Zope Public License,
@@ -10,30 +10,30 @@
 jQuery.noConflict();
 
 function init_topic_search() {
-    var topicsSearch=null, b=null, url=null,
-        ts='#gs-group-messages-topics-search';
+    var topicsSearch = null, b = null, url = null,
+        ts = '#gs-group-messages-topics-search';
 
     b = jQuery('base').attr('href');
-    if (b[b.length -1] != '/') {
+    if (b[b.length - 1] != '/') {
         b = b + '/';
     }
     url = b + 'gs-group-topics-ajax.html';
     topicsSearch = GSSearch(ts, url, 0, 6, {}, null);
     jQuery(ts).on('resultsloaded',
-                  function(){jQuery('abbr[role=tooltip]').tooltip();});
+                  function() {jQuery('abbr[role=tooltip]').tooltip();});
     jQuery('#gs-group-messages-base-tabs-buttons li:first-child a').click();
     topicsSearch.load();
 }
 
 // Connect to the DOM being ready, rather than the window loading, so the
 // topics can load while all the other assets are being downloaded
-jQuery(document).ready(function () {
+jQuery(document).ready(function() {
     gsJsLoader.with_module('/++resource++gs-search-base-js-min-20140313.js',
                            init_topic_search);
 });
 
-jQuery(window).load( function () {
-    var h=null, d=null, s=null;
+jQuery(window).load(function() {
+    var h = null, d = null, s = null;
     if (jQuery('#gs-group-messages-topics-postinginfo').length != 0) {
         h = jQuery('#gs-group-messages-topics-postinginfo-privacy').html();
         d = {animation: true,
