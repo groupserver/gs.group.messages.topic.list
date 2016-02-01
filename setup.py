00778e914bbd9201a6b850955e6bdde42386df7a
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-01 11:41:11 -0800

    Updating the product metadata

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.group.messages.topic.list'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
@@ -26,7 +27,7 @@ with codecs.open(os.path.join("docs", "HISTORY.rst"),
     long_description += '\n' + f.read()
 
 setup(
-    name='gs.group.messages.topic.list',
+    name=name,
     version=version,
     description="The Topics tab in a GroupServer group",
     long_description=long_description,
@@ -46,11 +47,11 @@ setup(
     keywords='groupserver, message, post, topic, topics',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://github.com/groupserver/gs.group.messages.topic.list/',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.messages',
-                        'gs.group.messages.topic'],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
